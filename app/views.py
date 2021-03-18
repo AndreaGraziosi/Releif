e9563dcd02dee2222f0f4135e7e02a92543a086d
Andrea_Graziosi <andreaggraziosi@gmail.com>
2021-03-18 13:04:24 -0700

    csv file

--- app/views.py
+++ app/views.py
@@ -17,24 +17,29 @@ def enter_problem():
 
 @app.route("/display_pose", methods=['POST'])
 def display_pose():
-    f = open("poses.csv", "r")
+    '''f = open("poses.csv", "r")
     reader = csv.reader(f)
     poses = list(reader)
     f.close()
-    #request.args.get('users_name')
-    #capture value entered in enter problem
-    # retrieve from the database
-    return render_template("/display_poses.html, poses=poses")
+    '''
+    return render_template("/display_pose.html")
 
 @app.route("/enter_poses", methods=['GET'])
 def enter_poses():
-    name = request.args.get('name')
-    ailment = request.args.get('ailment')
-    special_instructions = request.args.get('special_instructions')
-    pose_image = request.args.get('pose_image')
 
-    f = open("./poses.csv", "a")
-    f.write(",".join([name, ailment, special_instructions, pose_image]))
-    f.close()
-
+    myData = {
+    'name': request.args.get('name'),
+    'ailment': request.args.get('ailment'),
+    'special_instructions': request.args.get('special_instructions'),
+    'pose_image': request.args.get('pose_image')
+    }
+    myFile = open("./poses.csv", "a")
+    with myFile:
+        writer = csv.writer(myFile)
+        writer.writerows(myData)
+    myFile.close()
     return render_template("/enter_poses.html")
+
+@app.route("/login")
+def login():
+    return render_template("/login.html")
