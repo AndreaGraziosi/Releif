dfb4d6227cef29ccbf3119ab61e4502806ed6ec9
Andrea_Graziosi <andreaggraziosi@gmail.com>
2021-03-18 18:03:24 -0700

    implemented a dropdown

--- app/views.py
+++ app/views.py
@@ -17,28 +17,49 @@ def enter_problem():
 
 @app.route("/display_pose", methods=['POST'])
 def display_pose():
-    '''f = open("poses.csv", "r")
-    reader = csv.reader(f)
-    poses = list(reader)
-    f.close()
-    '''
+    
+    with open('poses.csv', newline='') as myFile:
+        reader = csv.reader(myFile)
+        for row in reader:
+            print(row)
+    myFile.close()
+    
     return render_template("/display_pose.html")
+
+
+my_yoga_pose = {
+        'Menstrual Pain':'Supta-Virasana, Supta-Baddha-Konasana, Paschimottanasana',
+        'Low Back Pain':'Supta-Padangusthasana, Adhomukha-Svanasana, Ustrasana',
+        'Eyes': 'Sirsasana, Sarvangasana, Uttanasana',
+        'Flat foot': 'Virasana, Mulabandhasana, Paschimotanasana',
+        'knees': 'Gomukhasana, Virasana, Janu-Sirsasana',
+        'Insomnia': 'Uttanasana, Paschimottanasana'
+    }
+
 
 @app.route("/enter_poses", methods=['GET'])
 def enter_poses():
+# This is to make sure the HTTP method is 
+# POST and not any other
+        '''if request.method == 'GET':
+        name =  request.args.get('name')
+        ailment = request.args.get('ailment')
+        special_instructions = request.args.get('special_instructions')
+        pose_image = request.args.get('pose_image')
+        fieldnames = [name, ailment, special_instructions, pose_image]
+        # TODO: enter name of your CSV file below
+        with open('poses.csv','w') as inFile:
+            writer = csv.DictWriter(inFile, fieldnames=fieldnames)
+            writer.writerow({'name': name, 'ailment': ailment, 'special_instructions':special_instructions, 'pose_image':pose_image})'''
+    
+        chosen_ailment = request.args.get('name')
 
-    myData = {
-    'name': request.args.get('name'),
-    'ailment': request.args.get('ailment'),
-    'special_instructions': request.args.get('special_instructions'),
-    'pose_image': request.args.get('pose_image')
-    }
-    myFile = open("./poses.csv", "a")
-    with myFile:
-        writer = csv.writer(myFile)
-        writer.writerows(myData)
-    myFile.close()
-    return render_template("/enter_poses.html")
+        context = {
+           'pose_names':my_yoga_pose.keys(),
+           'pose_cure':my_yoga_pose.get(chosen_ailment, "")
+        }
+
+        return render_template("/enter_poses.html", **context)
 
 @app.route("/login")
 def login():
